773da98f8277b955635d410831b7ffe21b22db2c
Mohammed Mahmood <mahmoodmohd829@gmail.com>
2025-10-25 17:33:30 +0530

    changed landingpage rename to lowercase

--- src/landingpagecomponent/landingpage.jsx
+++ src/landingpagecomponent/landingpage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../landingpagecomponent/Landingpagedesign.css';
+import '../landingpagecomponent/landingpagedesign.css';
 import bgImage1 from "../assets/green.jpg";
 import bgImage2 from "../assets/login.jpg";
 import bgImage3 from "../assets/testomonials.jpg";
